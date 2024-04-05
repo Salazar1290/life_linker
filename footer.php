4531e13593a8a99e7ded7b777dbbaedd68c66001
luckyfur <luckykispotta112@gmail.com>
2024-04-05 23:24:04 +0530

    added count on footer

--- footer.php
+++ footer.php
@@ -7,7 +7,8 @@
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 75px;
+  height: fit-content;
+  padding: 5px;
   background-color:#000000;
   color:white;
   text-align: center;
@@ -15,9 +16,18 @@
   </style>
 </head>
 <body>
+
   <div id="footer" >
   <b><center>COPYRIGHT © 2024<br>
   Blood Bank & Donation Management
+  <br>
+  <?php
+  $conn=mysqli_connect("localhost","root","","blood_donation") or die("Connection error");
+  $sql= "SELECT * FROM `donor_details`";
+  $result=mysqli_query($conn,$sql) or die("query unsuccessful.");
+  $num_rows = mysqli_num_rows($result);
+  ?>
+  DONORS ENROLLED :<b style='color: yellow;'> <?php echo $num_rows; ?></b>
   <br>
   ALL RIGHTS RESERVED.
   </center>
