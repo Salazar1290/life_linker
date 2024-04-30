e79fa872d15f55e7a752020659e147dd0bca4494
luckyfur <luckykispotta112@gmail.com>
2024-05-01 01:00:39 +0530

    fixed query page

--- admin/query.php
+++ admin/query.php
@@ -56,22 +56,7 @@ include 'conn.php';
 
       </div>
       <hr>
-      <script>
-      function clickme(){
-        if(confirm("Do you really Want to Read ?"))
-        {
-            document.getElementById("demo").innerHTML = "Read";
-            <?php
-            echo '<div class="alert alert-info alert_dismissible"><b><button type="button" class="close" data-dismiss="alert">&times;</button></b><b>Pending Request "Read".</b></div>';
 
-            $que_id = $_GET['id'];
-             $sql1="update contact_query set query_status='1' where  query_id={$que_id}";
-              $result=mysqli_query($conn,$sql1);
-            ?>
-        }
-      }
-
-      </script>
 
 
 
@@ -183,6 +168,21 @@ include 'conn.php';
        </form>
    <?php }
     ?>
+      <script>
+      function clickme(){
+        if(confirm("Do you really Want to Read ?"))
+        {
+            document.getElementById("demo").innerHTML = "Read";
+            <?php
+            echo '<div class="alert alert-info alert_dismissible"><b><button type="button" class="close" data-dismiss="alert">&times;</button></b><b>Pending Request "Read".</b></div>';
 
+            $que_id = $_GET['id'];
+             $sql1="update contact_query set query_status='1' where  query_id={$que_id}";
+              $result=mysqli_query($conn,$sql1);
+            ?>
+        }
+      }
+
+      </script>
 </body>
 </html>
